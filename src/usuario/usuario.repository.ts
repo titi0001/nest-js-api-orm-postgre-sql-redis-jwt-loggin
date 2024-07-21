588475b573444bc4c94dd116521d8df92d9fe9dc
Thiago Pereira <titiura@gmail.com>
2024-07-21 14:42:28 -0300

    feat: create user entity and  repository

---
+++ src/usuario/usuario.repository.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { UsuarioEntity } from './usuario.entity';
+
+@Injectable()
+export class UsuarioRepository {
+  private usuarios: UsuarioEntity[] = [];
+
+  async salvar(usuario: UsuarioEntity) {
+    this.usuarios.push(usuario);
+  }
+
+  async listarUsuarios() {
+    return this.usuarios;
+  }
+
+  async buscarPorEmail(email: string) {
+    const possivelUsuario = this.usuarios.find(
+      (usuario) => usuario.email === email,
+    );
+    return possivelUsuario !== undefined;
+  }
+}
