aceba5a79505f4d6f6d24e65d8e008e975a909b1
Thiago Pereira <titiura@gmail.com>
2024-07-21 14:40:23 -0300

    feat: add usuario module and Produto Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsuarioModule } from './usuario/usuario.module';
+import { ProdutoModule } from './produto/produto.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsuarioModule, ProdutoModule],
 })
 export class AppModule {}
