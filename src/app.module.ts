9771d487b197e43b220017593f6644bae1bb9cc9
Thiago Pereira <titiura@gmail.com>
2024-07-22 19:03:48 -0300

    feat: add config module to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,15 @@ import { UsuarioModule } from './usuario/usuario.module';
 import { ProdutoModule } from './produto/produto.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostgresConfigService } from './config/postgres.config.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     UsuarioModule,
     ProdutoModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRootAsync({
       useClass: PostgresConfigService,
       inject: [PostgresConfigService],
