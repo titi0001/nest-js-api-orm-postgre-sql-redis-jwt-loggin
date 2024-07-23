f84202773eb7f49ef15179945592f5d4d0814e09
Thiago Pereira <titiura@gmail.com>
2024-07-22 22:14:32 -0300

    feat: create user entity

--- src/usuario/usuario.entity.ts
+++ src/usuario/usuario.entity.ts
@@ -1,6 +1,42 @@
+import {
+  Entity,
+  Column,
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'usuarios' })
 export class UsuarioEntity {
+  @PrimaryGeneratedColumn('uuid', { name: 'id' })
   id: string;
+
+  @Column({
+    name: 'nome',
+    length: 70,
+    nullable: false,
+  })
   nome: string;
+
+  @Column({
+    name: 'email',
+    length: 100,
+    nullable: false,
+  })
   email: string;
+
+  @Column({
+    name: 'senha',
+    length: 255,
+    nullable: false,
+  })
   senha: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: string;
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: string;
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt: string;
 }
