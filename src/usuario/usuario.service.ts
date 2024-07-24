422b8155d7d33c9d4dcd28ba4a4c4e10b730ae95
Thiago Pereira <titiura@gmail.com>
2024-07-24 18:33:12 -0300

    feat: create user service

---
+++ src/usuario/usuario.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UsuarioEntity } from './usuario.entity';
+import { Repository } from 'typeorm';
+import { ListaUsuariosDTO } from './dto/ListaUsuarios.dto';
+import { AtualizaUsuarioDTO } from './dto/AtualizaUsuario.dto';
+
+@Injectable()
+export class UsuarioService {
+  constructor(
+    @InjectRepository(UsuarioEntity)
+    private readonly usuarioRepository: Repository<UsuarioEntity>,
+  ) {}
+
+  async listaUsuarios() {
+    const usuariosSalvos = await this.usuarioRepository.find();
+    const usuariosLista = usuariosSalvos.map(
+      (usuario) => new ListaUsuariosDTO(usuario.id, usuario.nome),
+    );
+    return usuariosLista;
+  }
+
+  async criaUsuario(usuarioEntity: UsuarioEntity) {
+    return await this.usuarioRepository.save(usuarioEntity);
+  }
+
+  async atualizaUsuario(id: string, usuarioEntity: AtualizaUsuarioDTO) {
+    await this.usuarioRepository.update(id, usuarioEntity);
+  }
+
+  async deletaUsuario(id: string) {
+    await this.usuarioRepository.delete(id);
+  }
+}
