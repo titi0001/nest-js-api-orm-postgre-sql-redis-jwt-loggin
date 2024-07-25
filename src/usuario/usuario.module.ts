39b6a0abb50f1319c4c4174940df9900b6af2ef5
Thiago Pereira <titiura@gmail.com>
2024-07-25 19:22:39 -0300

    feat: add exports User service and decorators Global

--- src/usuario/usuario.module.ts
+++ src/usuario/usuario.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsuarioController } from './usuario.controller';
 import { UsuarioRepository } from './usuario.repository';
@@ -6,9 +6,11 @@ import { EmailUnicoValidator } from './validacao/emailUnico.validator';
 import { UsuarioService } from './usuario.service';
 import { UsuarioEntity } from './usuario.entity';
 
+@Global()
 @Module({
   imports: [TypeOrmModule.forFeature([UsuarioEntity])],
   controllers: [UsuarioController],
   providers: [UsuarioService, UsuarioRepository, EmailUnicoValidator],
+  exports: [UsuarioService],
 })
 export class UsuarioModule {}
