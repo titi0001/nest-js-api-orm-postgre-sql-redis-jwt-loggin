f05c146752ad5ad3b462e74ddeff5ba27d00928b
Thiago Pereira <titiura@gmail.com>
2024-07-26 00:28:31 -0300

    fix: imports product image and product characteristic

--- src/produto/produto.module.ts
+++ src/produto/produto.module.ts
@@ -4,9 +4,17 @@ import { ProdutoRepository } from './produto.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProdutoEntity } from './produto.entity';
 import { ProdutoService } from './produto.service';
+import { ProdutoCaracteristicaEntity } from './produto-caracteristica.entity';
+import { ProdutoImagemEntity } from './produto-imagem.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProdutoEntity])],
+  imports: [
+    TypeOrmModule.forFeature([
+      ProdutoEntity,
+      ProdutoCaracteristicaEntity,
+      ProdutoImagemEntity,
+    ]),
+  ],
   controllers: [ProdutoController],
   providers: [ProdutoRepository, ProdutoService],
 })
