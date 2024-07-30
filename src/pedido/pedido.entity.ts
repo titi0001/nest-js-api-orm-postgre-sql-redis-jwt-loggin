918e08dd4d78e4976bce0e24f8805c0fb6674cf4
Thiago Pereira <titiura@gmail.com>
2024-07-30 20:20:45 -0300

    feat: create orders entity

---
+++ src/pedido/pedido.entity.ts
@@ -0,0 +1,40 @@
+import {
+  Entity,
+  Column,
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+  ManyToOne,
+} from 'typeorm';
+import { StatusPedido } from './enum/statusPedido.enum';
+import { UsuarioEntity } from '../usuario/usuario.entity';
+
+@Entity({ name: 'pedidos' })
+export class PedidoEntity {
+  @PrimaryGeneratedColumn('uuid', { name: 'id' })
+  id: string;
+
+  @Column({
+    name: 'valor_total',
+    nullable: false,
+  })
+  valorTotal: number;
+
+  @Column({
+    name: 'status',
+    enum: StatusPedido,
+    nullable: false,
+  })
+  status: StatusPedido;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: string;
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: string;
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt: string;
+
+  @ManyToOne(() => UsuarioEntity, (usuario) => usuario.pedidos)
+  usuario: UsuarioEntity;
+}
