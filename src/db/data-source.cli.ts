1716de681f44916142e4aeab28692f559f720244
Thiago Pereira <titiura@gmail.com>
2024-07-29 23:04:18 -0300

    feat: create Data Source

---
+++ src/db/data-source.cli.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/migrations/*{.ts,.js}'],
+};
+
+const dataSource = new DataSource(dataSourceOptions);
+
+export default dataSource;
