49c1aa17b4a48ee71a091074e65ab294ed3e34d7
Thiago Pereira <titiura@gmail.com>
2024-07-31 22:30:23 -0300

    feat: create create orders

--- src/pedido/pedido.controller.ts
+++ src/pedido/pedido.controller.ts
@@ -1,13 +1,20 @@
-import { Controller, Post, Query, Get } from '@nestjs/common';
+import { Controller, Post, Query, Get, Body } from '@nestjs/common';
 import { PedidoService } from './pedido.service';
+import { CriaPedidoDTO } from './dto/criaPedido.dto';
 
 @Controller('pedidos')
 export class PedidoController {
   constructor(private readonly pedidoService: PedidoService) {}
 
   @Post()
-  CriaPedido(@Query('usuarioId') usuarioId: string) {
-    const pedidoCriado = this.pedidoService.cadastraPedido(usuarioId);
+  CriaPedido(
+    @Query('usuarioId') usuarioId: string,
+    @Body() dadosDoPedido: CriaPedidoDTO,
+  ) {
+    const pedidoCriado = this.pedidoService.cadastraPedido(
+      usuarioId,
+      dadosDoPedido,
+    );
 
     return pedidoCriado;
   }
