56fd5ac94343bc6cb2d749d20c520aa7b7fc8bb4
Thiago Pereira <titiura@gmail.com>
2024-08-01 22:41:26 -0300

    Refactor: update user apply extends PartialType im class update user DTO

--- src/usuario/dto/AtualizaUsuario.dto.ts
+++ src/usuario/dto/AtualizaUsuario.dto.ts
@@ -1,19 +1,4 @@
-import { IsEmail, IsNotEmpty, IsOptional, MinLength } from 'class-validator';
-import { EmailUnico } from '../validacao/emailUnico.validator';
+import { CriaUsuarioDTO } from './CriaUsuario.dto';
+import { PartialType } from '@nestjs/mapped-types';
 
-export class AtualizaUsuarioDTO {
-  @IsNotEmpty({ message: 'O nome não pode ser vazio' })
-  @IsOptional()
-  nome: string;
-
-  @IsEmail(undefined, { message: 'E-mail inválido' })
-  @IsOptional()
-  @EmailUnico({
-    message: 'E-mail já cadastrado',
-  })
-  email: string;
-
-  @MinLength(6, { message: 'Senha deve ter no mínimo 6 caracteres' })
-  @IsOptional()
-  senha: string;
-}
+export class AtualizaUsuarioDTO extends PartialType(CriaUsuarioDTO) {}
