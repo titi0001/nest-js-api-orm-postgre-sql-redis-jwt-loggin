270e6a5e5d2b7667ab668320e9451983ff70e84b
Thiago Pereira <titiura@gmail.com>
2024-09-03 10:57:16 -0300

    Feat: add regex validator to pass

--- src/modulos/usuario/dto/CriaUsuario.dto.ts
+++ src/modulos/usuario/dto/CriaUsuario.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+import { IsEmail, IsNotEmpty, Matches, MinLength } from 'class-validator';
 import { EmailUnico } from '../validacao/emailUnico.validator';
 
 export class CriaUsuarioDTO {
@@ -12,5 +12,12 @@ export class CriaUsuarioDTO {
   email: string;
 
   @MinLength(6, { message: 'Senha deve ter no mínimo 6 caracteres' })
+  @Matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{6,30}$/,
+    {
+      message:
+        'A senha deve conter pelo menos uma letra minúscula, uma letra maiúscula, um dígito, um caractere especial e ter entre 6 e 30 caracteres',
+    },
+  )
   senha: string;
 }
