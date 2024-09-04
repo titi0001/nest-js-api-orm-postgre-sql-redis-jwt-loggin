e3f4f6ede1d10177d4c0be0b7df3f4f0f19eeb60
Thiago Pereira <titiura@gmail.com>
2024-09-03 21:09:32 -0300

    Feat: add hash pipe to create user

--- src/modulos/usuario/usuario.controller.ts
+++ src/modulos/usuario/usuario.controller.ts
@@ -9,29 +9,30 @@ import {
   UseInterceptors,
 } from '@nestjs/common';
 import { CriaUsuarioDTO } from './dto/CriaUsuario.dto';
-import { UsuarioEntity } from './usuario.entity';
-import { v4 as uuid } from 'uuid';
+
 import { ListaUsuariosDTO } from './dto/ListaUsuarios.dto';
 import { AtualizaUsuarioDTO } from './dto/AtualizaUsuario.dto';
 import { UsuarioService } from './usuario.service';
 import { CacheInterceptor } from '@nestjs/cache-manager';
+import { HashPassPipe } from '../../resources/pipes/hash-pass.pipe';
 
 @Controller('/usuarios')
 export class UsuarioController {
   constructor(private readonly usuarioService: UsuarioService) {}
 
   @Post()
-  async criaUsuario(@Body() dadosDoUsuario: CriaUsuarioDTO) {
-    const usuarioEntity = new UsuarioEntity();
-    usuarioEntity.nome = dadosDoUsuario.nome;
-    usuarioEntity.email = dadosDoUsuario.email;
-    usuarioEntity.senha = dadosDoUsuario.senha;
-    usuarioEntity.id = uuid();
+  async criaUsuario(
+    @Body() { ...dadosDoUsuario }: CriaUsuarioDTO,
+    @Body('senha', HashPassPipe) senhaHash: string,
+  ) {
+    const usuarioCriado = await this.usuarioService.criaUsuario({
+      ...dadosDoUsuario,
+      senha: senhaHash,
+    });
 
-    this.usuarioService.criaUsuario(usuarioEntity);
     return {
-      usuario: new ListaUsuariosDTO(usuarioEntity.id, usuarioEntity.nome),
-      mensagem: 'Usuário criado com sucesso',
+      message: 'usuário criado com sucesso',
+      usuario: new ListaUsuariosDTO(usuarioCriado.id, usuarioCriado.nome),
     };
   }
 
