1583775aca2b9ce919ce3cef10bfb9886f054ba9
Thiago Pereira <titiura@gmail.com>
2024-09-09 09:49:16 -0300

    Feat: imports JWTModule in auth module

--- src/modulos/autenticacao/autenticacao.module.ts
+++ src/modulos/autenticacao/autenticacao.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AutenticacaoService } from './autenticacao.service';
 import { AutenticacaoController } from './autenticacao.controller';
 import { UsuarioModule } from '../usuario/usuario.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UsuarioModule],
+  imports: [
+    UsuarioModule,
+    JwtModule.register({
+      global: true,
+      secret: 'SECRET_KEY',
+      signOptions: {
+        expiresIn: '1d',
+      },
+    }),
+  ],
   controllers: [AutenticacaoController],
   providers: [AutenticacaoService],
 })
