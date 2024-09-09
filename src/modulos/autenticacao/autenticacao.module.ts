15c1308ea857912f3ef2665e655bb91ff3c89843
Thiago Pereira <titiura@gmail.com>
2024-09-09 13:03:30 -0300

    Feat: add secret JWT in auth module

--- src/modulos/autenticacao/autenticacao.module.ts
+++ src/modulos/autenticacao/autenticacao.module.ts
@@ -3,16 +3,22 @@ import { AutenticacaoService } from './autenticacao.service';
 import { AutenticacaoController } from './autenticacao.controller';
 import { UsuarioModule } from '../usuario/usuario.module';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     UsuarioModule,
-    JwtModule.register({
-      global: true,
-      secret: 'SECRET_KEY',
-      signOptions: {
-        expiresIn: '1d',
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => {
+        return {
+          secret: configService.get<string>('SECRET_JWT'),
+          signOptions: {
+            expiresIn: '1d',
+          },
+        };
       },
+      inject: [ConfigService],
+      global: true,
     }),
   ],
   controllers: [AutenticacaoController],
