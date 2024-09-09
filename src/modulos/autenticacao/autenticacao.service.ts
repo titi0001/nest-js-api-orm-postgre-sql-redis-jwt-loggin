e32c4da085444ce5ba3c9a9635c6c233d845b1e8
Thiago Pereira <titiura@gmail.com>
2024-09-09 09:49:51 -0300

    Feat: create method login in auth service

--- src/modulos/autenticacao/autenticacao.service.ts
+++ src/modulos/autenticacao/autenticacao.service.ts
@@ -1,8 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsuarioService } from '../usuario/usuario.service';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+
+interface UsuarioPayload {
+  sub: string;
+  email: string;
+}
 
 @Injectable()
 export class AutenticacaoService {
-  login(email: string, senha: string) {
-    console.log(email, senha);
+  constructor(
+    private usuarioService: UsuarioService,
+    private jwtService: JwtService,
+  ) {}
+  async login(email: string, senhaInserida: string) {
+    const usuario = await this.usuarioService.buscaPorEmail(email);
+
+    const usuarioAutenticado = await bcrypt.compare(
+      senhaInserida,
+      usuario.senha,
+    );
+
+    if (!usuarioAutenticado) {
+      throw new UnauthorizedException('Email ou senha inválidos');
+    }
+
+    const payload: UsuarioPayload = {
+      sub: usuario.id,
+      email: usuario.email,
+    };
+
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
   }
 }
