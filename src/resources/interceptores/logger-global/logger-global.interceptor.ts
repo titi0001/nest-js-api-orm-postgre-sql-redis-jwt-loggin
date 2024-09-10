9d6977d28e0584ec8390e5a1dd20b4ec7afd627a
Thiago Pereira <titiura@gmail.com>
2024-09-09 22:17:15 -0300

    Feat: add LoggerGlobalInterceptor for logging routes accessed by authenticated users

--- src/resources/interceptores/logger-global/logger-global.interceptor.ts
+++ src/resources/interceptores/logger-global/logger-global.interceptor.ts
@@ -1,9 +1,28 @@
-import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import {
+  CallHandler,
+  ConsoleLogger,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { Observable, tap } from 'rxjs';
+import { ReqComUsuario } from '../../../../src/modulos/autenticacao/autenticacao/autenticacao.guard';
 
 @Injectable()
 export class LoggerGlobalInterceptor implements NestInterceptor {
+  constructor(private logger: ConsoleLogger) {}
+
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    return next.handle();
+    const contextHttp = context.switchToHttp();
+    const request = contextHttp.getRequest<Request | ReqComUsuario>();
+
+    return next.handle().pipe(
+      tap(() => {
+        if ('usuario' in request) {
+          this.logger.log(`Rota acessada pelo usuario ${request.usuario.sub}`);
+        }
+      }),
+    );
   }
 }
